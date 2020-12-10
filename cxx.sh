acd491767e0c37d7bdd473f4fd2008df97435622
anthonybrunasso <AnthonyBrunasso@gmail.com>
2020-12-09 18:50:04 -0800

    cxx.sh

---
+++ cxx.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+source ./bash_constants.sh
+
+if [ $# -eq 0 ]; then
+  echo 'Usage: $0 <cc_file>'
+  exit 1
+fi
+
+FILEEXT=.${1##*.}
+APP=$BIN_DIR/`basename -s $FILEEXT $1`
+
+if [ -n "$unix" ]; then
+  echo Building with $CXX $CXXFLAGS
+  time $CXX $CXXFLAGS $1 -I src/ -lX11 -lEGL -lGL -lpthread -o $APP
+else
+  time $CXX -g $CXXFLAGS $1 -I . -I src/ -ObjC++ -L bin/ -ldl -o $APP -stdlib=libc++ -Wno-format -Wno-deprecated-declarations
+  # For teeny weeny builds '-Os -flto'
+  # O0 takes about half a second off the compile time.
+fi
+
+# If more than one argument, and build succeeded
+if [ $? -eq 0 -a $# -gt 1 ]; then
+  # pop the first argument
+  shift
+  # pass remaining arguments to the app
+  ./$APP $@
+fi
